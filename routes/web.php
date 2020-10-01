aca56564abd5afd9632a44024a9b9577cc7c787e
raphaelpavani <raphaelpavani@gmail.com>
2020-10-01 20:04:33 -0300

    tentando entender pq a rota ta errada

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,62 +13,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('products', 'ProductController@index')->name('products.index');
+//Route::get('products', [ProductController::class, 'index']);
 
-//nomenado rotas e redirecionando
-Route::get('redirect3', function () {
-    return redirect()->route('url.name');
-});
-
-Route::get('/nome-url', function () {
-    return 'hey hey hey';
-})->name('url.name');
-
-Route::view('/view', 'welcome', ['id' => 'teste']);
-
-
-Route::redirect('/redirect1', '/redirect2');
-
-// Route::get('redirect1', function () {
-//     return redirect('/redirect2');
+// //nomenado rotas e redirecionando
+// Route::get('redirect3', function () {
+//     return redirect()->route('url.name');
 // });
 
-Route::get('redirect2', function () {
-    return 'Redirect 02';
-});
+// Route::get('/nome-url', function () {
+//     return 'hey hey hey';
+// })->name('url.name');
 
-Route::get('/produtos/{idProducts?}/posts', function ($idProducts = "") {
-    return "Posts da categoria ${idProducts}";
-});
+// Route::view('/view', 'welcome', ['id' => 'teste']);
 
 
+// Route::redirect('/redirect1', '/redirect2');
 
-Route::get('/categoria/{flag}/posts', function ($flag) {
-    return "Posts da categoria ${flag}";
-});
+// // Route::get('redirect1', function () {
+// //     return redirect('/redirect2');
+// // });
 
+// Route::get('redirect2', function () {
+//     return 'Redirect 02';
+// });
 
-Route::get('/categoria/{flag}', function ($flag) {
-    return "Produtos da categoria ${flag}";
-});
-
-
-
-Route::match(['post', 'get'], '/match', function () {
-    return 'match';
-}); //aceita as rotas que eu indicar o verbo
-
-Route::any('/any', function () {
-    return 'any';
-}); // aceita qqr rota
+// Route::get('/produtos/{idProducts?}/posts', function ($idProducts = "") {
+//     return "Posts da categoria ${idProducts}";
+// });
 
 
-Route::get('/empresa', function () {
-    return view('site.contact');
-});
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/contato', function () {
-    return 'contato';
-});
+// Route::get('/categoria/{flag}/posts', function ($flag) {
+//     return "Posts da categoria ${flag}";
+// });
+
+
+// Route::get('/categoria/{flag}', function ($flag) {
+//     return "Produtos da categoria ${flag}";
+// });
+
+
+
+// Route::match(['post', 'get'], '/match', function () {
+//     return 'match';
+// }); //aceita as rotas que eu indicar o verbo
+
+// Route::any('/any', function () {
+//     return 'any';
+// }); // aceita qqr rota
+
+
+// Route::get('/empresa', function () {
+//     return view('site.contact');
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/contato', function () {
+//     return 'contato';
+// });
