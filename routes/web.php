4a7f65b6fc0b427ecac8215e91a0946862f24e44
raphaelpavani <raphaelpavani@gmail.com>
2020-10-01 12:45:28 -0300

    Aprendi rotas e views e paths

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/empresa', function () {
+    return view('site.contact');
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/contato', function () {
+    return 'contato';
 });
