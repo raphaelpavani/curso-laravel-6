dc76e5b99c51a385cc45069b6dbcbcba0da82333
raphaelpavani <raphaelpavani@gmail.com>
2020-10-01 17:27:14 -0300

    Aprendendo sobre rotas redirecionamento e nomes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,54 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+//nomenado rotas e redirecionando
+Route::get('redirect3', function () {
+    return redirect()->route('url.name');
+});
+
+Route::get('/nome-url', function () {
+    return 'hey hey hey';
+})->name('url.name');
+
+Route::view('/view', 'welcome', ['id' => 'teste']);
+
+
+Route::redirect('/redirect1', '/redirect2');
+
+// Route::get('redirect1', function () {
+//     return redirect('/redirect2');
+// });
+
+Route::get('redirect2', function () {
+    return 'Redirect 02';
+});
+
+Route::get('/produtos/{idProducts?}/posts', function ($idProducts = "") {
+    return "Posts da categoria ${idProducts}";
+});
+
+
+
+Route::get('/categoria/{flag}/posts', function ($flag) {
+    return "Posts da categoria ${flag}";
+});
+
+
+Route::get('/categoria/{flag}', function ($flag) {
+    return "Produtos da categoria ${flag}";
+});
+
+
+
+Route::match(['post', 'get'], '/match', function () {
+    return 'match';
+}); //aceita as rotas que eu indicar o verbo
+
+Route::any('/any', function () {
+    return 'any';
+}); // aceita qqr rota
+
+
 Route::get('/empresa', function () {
     return view('site.contact');
 });
